be6c3c12dc43ba1bf56a6c65084b363d64eb0649
Drew Naylor <DrewNaylor@users.noreply.github.com>
2021-08-20 09:23:48 -0400

    Add boilerplate text that was missing along with...
    text that clarifies that it was missing and that I assume it's ok to put it there because the license and copyrights in the LICENSE file are assumed to apply to everything unless otherwise specified.

--- src/unix/MyClass.cs
+++ src/unix/MyClass.cs
@@ -1,4 +1,39 @@
-﻿using System;
+﻿// WeastroShell - A free/libre open source software desktop environment for Windows and Linux.
+// This project is a fork of Abanu Desktop: https://github.com/abanu-desktop/abanu
+// WeastroShell is not associated with Abanu Desktop or its developers beyond being a fork.
+//
+// Original code is, I assume based on the file "LICENSE", Copyright (C) 2015 Sebastian Loncar
+// Modifications to this file are Copyright (C) 2021 Drew Naylor.
+// (Note that the copyright years include the years left out by the hyphen.)
+//
+// This project is under the MIT License.
+// See the file "LICENSE" in the project root directory for more information. Below is a copy of the license:
+//
+//   Permission is hereby granted, free of charge, to any person obtaining a copy
+//   of this software and associated documentation files (the "Software"), to deal
+//   in the Software without restriction, including without limitation the rights
+//   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//   copies of the Software, and to permit persons to whom the Software is
+//   furnished to do so, subject to the following conditions:
+// 
+//   The above copyright notice and this permission notice shall be included in all
+//   copies or substantial portions of the Software.
+// 
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//   SOFTWARE.
+//
+// Drew Naylor put this license/copyright boilerplate text here for clarity because it was missing in
+// the original code, though it's assumed that doing so is ok because the project itself was under
+// the MIT License and as such both the license and copyrights should apply to all the files
+// unless otherwise specified.
+//
+
+using System;
 using System.IO;
 using Gdk;
 using Gtk;
